a6ef799cc6c9e0b16ea688a673d13279c70bb16c
Julio Sueiras <juliosueiras@gmail.com>
2023-07-28 12:10:01 -0400

    Update app.js

--- themes/material/app.js
+++ themes/material/app.js
@@ -78,10 +78,6 @@ function list(path){
 	   <li class="mdui-list-item th"> 
 	    <div class="mdui-col-xs-12 mdui-col-sm-7">
 	     Name
-	<i class="mdui-icon material-icons icon-sort" data-sort="name" data-order="more">expand_more</i>
-	    </div> 
-	    <div class="mdui-col-sm-3 mdui-text-right">
-	     Date Modified
 	<i class="mdui-icon material-icons icon-sort" data-sort="date" data-order="downward">expand_more</i>
 	    </div> 
 	    <div class="mdui-col-sm-2 mdui-text-right">
