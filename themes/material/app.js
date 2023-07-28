7c0b3c00f4a9cb5c4b4bfdae68358e37e5acca81
Julio Sueiras <juliosueiras@gmail.com>
2023-07-28 13:05:40 -0400

    Update app.js

--- themes/material/app.js
+++ themes/material/app.js
@@ -152,10 +152,17 @@ function list_files(path,files){
 	            p += "?a=view";
 	            c += " view";
             }
+	    var parsedName = "";
+	    try {
+	      parsedName = decodeURIComponent(item.name)
+	    } catch(err) {
+	      console.log(item.name)
+	      parsedName = item.name
+	    }
             html += `<li class="mdui-list-item file mdui-ripple" target="_blank"><a gd-type="${item.mimeType}" href="${p}" class="${c}">
 	          <div class="mdui-col-xs-12 mdui-col-sm-7 mdui-text-truncate">
 	          <i class="mdui-icon material-icons">insert_drive_file</i>
-	            ${item.name}
+	            ${parsedName}
 	          </div>
 	          <div class="mdui-col-sm-2 mdui-text-right">${item['size']}</div>
 	          </a>
